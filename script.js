9eca50923282cb86a8a6bb42c102ed6889db4293
Divya Shrestha <divya.shrestha@centria.fi>
2025-01-18 21:34:37 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const navLinks = document.querySelectorAll('nav ul li a');
+
+    
+    navLinks.forEach(link => {
+        link.addEventListener('click', (e) => {
+            e.preventDefault();
+            const targetId = link.getAttribute('href').substring(1);
+            document.getElementById(targetId).scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
+    });
+});
